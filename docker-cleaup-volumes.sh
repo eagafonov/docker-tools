86931e49af09f053a1ecea0bc0debb927231464e
Eugene Agafonov <e.a.agafonov@gmail.com>
2015-04-28 09:20:28 +0000

    Initial commit

---
+++ docker-cleaup-volumes.sh
@@ -0,0 +1,77 @@
+#! /bin/bash
+
+#usage: sudo ./docker-cleanup-volumes.sh [--dry-run]
+
+dockerdir=/var/lib/docker
+volumesdir=${dockerdir}/volumes
+vfsdir=${dockerdir}/vfs/dir
+allvolumes=()
+dryrun=true
+
+function delete_volumes() {
+  targetdir=$1
+  echo
+  if [[ ! -d ${targetdir} ]]; then
+        echo "Directory ${targetdir} does not exist, skipping."
+        return
+  fi
+  echo "Delete unused volume directories from $targetdir"
+  for dir in $(ls -d ${targetdir}/* 2>/dev/null)
+  do
+        dir=$(basename $dir)
+        if [[ "${dir}" =~ [0-9a-f]{64} ]]; then
+                if [[ ${allvolumes[@]} =~ "${dir}" ]]; then
+                        echo In use ${dir}
+                else
+                        if [ "${dryrun}" = false ]; then
+                                echo Deleting ${dir}
+                                rm -rf ${targetdir}/${dir}
+                        else
+                                echo Would have deleted ${dir}
+                        fi
+                fi
+        else
+                echo Not a volume ${dir}
+        fi
+  done
+}
+
+if [ $UID != 0 ]; then
+    echo "You need to be root to use this script."
+    exit 1
+fi
+
+docker_bin=$(which docker.io || which docker)
+if [ -z "$docker_bin" ] ; then
+    echo "Please install docker. You can install docker by running \"wget -qO- https://get.docker.io/ | sh\"."
+    exit 1
+fi
+
+# docker_bin="$docker_bin -H tcp://192.168.81.180:2375"
+
+if [ "$1" = "--do-it" ]; then
+        dryrun=false
+else if [ -n "$1" ]; then
+        echo "Cleanup docker volumes: remove unused volumes."
+        echo "Usage: ${0##*/} [--do-it]"
+        echo "   --do-it: do not a dry run, otherwise display what would get removed."
+        exit 1
+fi
+fi
+
+
+#All volumes from all containers
+for container in `${docker_bin} ps -a -q --no-trunc`; do
+        #add container id to list of volumes, don't think these
+        #ever exists in the volumesdir but just to be safe
+        allvolumes+=${container}
+        #add all volumes from this container to the list of volumes
+        for vid in `${docker_bin} inspect --format='{{range $vol, $path := .Volumes}}{{$path}}{{"\n"}}{{end}}' ${container}`; do
+                if [[ "${vid##*/}" =~ [0-9a-f]{64} ]]; then
+                        allvolumes+=("${vid##*/}")
+                fi
+        done
+done
+
+delete_volumes ${volumesdir}
+delete_volumes ${vfsdir}
